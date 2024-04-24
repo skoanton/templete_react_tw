a28965679dcd627b14388645a9c7ba415aae7593
skoanton <antonhakanssonn@gmail.com>
2024-04-24 18:10:59 +0200

    inital setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black h-screen flex justify-center items-center">
+      <h1 className="text-4xl text-green-500">Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
